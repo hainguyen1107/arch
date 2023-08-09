a89bbc4f4e31e801cbebadf2ea6bb317ba0decfd
hainguyen1107 <72888088+hainguyen1107@users.noreply.github.com>
2023-08-10 00:05:08 +0700

    Update Archinstall.sh

--- Archinstall.sh
+++ Archinstall.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
+echo "-------------------------------------------------"
+echo "Setting up mirrors for optimal download - Singapore Only"
+echo "-------------------------------------------------"
 pacman-key --init
 pacman-key --populate
 pacman -Syyy
