899464cf62484ddcaabb2cb5a4afcbcae95d395f
hainguyen1107 <72888088+hainguyen1107@users.noreply.github.com>
2023-08-10 15:48:21 +0700

    Update Archinstall.sh

--- Archinstall.sh
+++ Archinstall.sh
@@ -11,7 +11,7 @@ pacman-key --populate
 pacman -Syyy
 pacman -S pacman-contrib --noconfirm --needed
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://archlinux.org/mirrorlist/?country=SG&protocol=http&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -s "https://archlinux.org/mirrorlist/?country=FR&country=GB&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 -
 
 echo -e "\nInstalling prereqs...\n"
 pacman -S --noconfirm gptfdisk
