b4658e8f7932c0c7724b1ce644eafa95fd1270fd
hainguyen1107 <72888088+hainguyen1107@users.noreply.github.com>
2023-08-10 22:39:37 +0700

    Update 1 - Base.sh

--- 1 - Base.sh
+++ 1 - Base.sh
@@ -72,10 +72,13 @@ for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
     pacman -Sy "$PKG" --noconfirm --needed
 done
+
 # Configure audio
-mkdir -p /home/serotonin/.config/pipeware
-cp -r /usr/share/pipewire /home/serotonin/.config/
-sed -i ‘/resample.quality/s/#//; /resample.quality/s/4/15/’ /home/serotonin/.config/pipewire/{client.conf,pipewire-pulse.conf}
+echo "Please enter your username!"
+read user
+mkdir -p /home/$user/.config/pipeware
+cp -r /usr/share/pipewire /home/$user/.config/
+sed -i ‘/resample.quality/s/#//; /resample.quality/s/4/15/’ /home/$user/.config/pipewire/{client.conf,pipewire-pulse.conf}
 
 echo
 echo "Done!"
