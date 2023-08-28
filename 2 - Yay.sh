cb754e498892a577946f2f2a924d76efb35dc68f
hainguyen1107 <72888088+hainguyen1107@users.noreply.github.com>
2023-08-28 16:20:37 +0700

    Update 2 - Yay.sh

--- 2 - Yay.sh
+++ 2 - Yay.sh
@@ -105,76 +105,8 @@ echo "GTK_IM_MODULE=fcitx" >> ~/.bashrc
 echo "QT_IM_MODULE=fcitx" >> ~/.bashrc
 echo "XMODIFIERS=@im=fcitx" >> ~/.bashrc
 
-echo "Configuring KDE Plasma!!!"
-# Disable Recent Documents tracking (sensitive file revealed!)
-kwriteconfig5 --file $HOME/.config/kdeglobals --group RecentDocuments --key UseRecent false
-
-# Use Google-chrome browser for http and https URLs.
-kwriteconfig5 --file $HOME/.config/kdeglobals --group 'General' --key 'BrowserApplication' 'google-chrome.desktop'
-
-# Use fastest animation speed.
-kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key 'AnimationSpeed' '0'
-
-# Disable sliding popup effect.
-kwriteconfig5 --file $HOME/.config/kwinrc --group Plugins --key 'slidingpopupsEnabled' --type 'bool' 'false'
-
-# Turn off alert noise when AC adapter is unplugged.
-kwriteconfig5 --file $HOME/.config/powerdevil.notifyrc --group 'Event/unplugged' --key 'Action' ''
-
-# Turn off alert noise when trash is emptied.
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/Trash: emptied' --key 'Action' ''
-
-# Turn off alert noises for warnings and errors (popup instead).
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/catastrophe' --key 'Action' 'Popup'
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/fatalerror' --key 'Action' 'Popup'
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/messageCritical' --key 'Action' 'Popup'
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/messageInformation' --key 'Action' 'Popup'
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/messageWarning' --key 'Action' 'Popup'
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/messageboxQuestion' --key 'Action' 'Popup'
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/notification' --key 'Action' 'Popup'
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/printerror' --key 'Action' 'Popup'
-kwriteconfig5 --file  $HOME/.config/plasma_workspace.notifyrc --group 'Event/warning' --key 'Action' 'Popup'    
-
-# Turn off alerts for console bells.
-kwriteconfig5 --file  $HOME/.config/konsole.notifyrc --group 'Event/BellInvisible' --key 'Action' ''
-kwriteconfig5 --file  $HOME/.config/konsole.notifyrc --group 'Event/BellVisible' --key 'Action' ''
-
-# Show hidden files
-kwriteconfig5 --file $HOME/.config/kdeglobals --group 'KFileDialog Settings' --key 'Show hidden files' true
-
-# Disable kwallet
-kwriteconfig5 --file $HOME/.config/kwalletrc --group "Wallet" --key "Enabled" "false"
-
-# Narrower window drop shadows.
-kwriteconfig5 --file breezerc --group 'Common' --key 'ShadowSize' 'ShadowSmall'
-
-# Don't show media controls on the lock screen.
-kwriteconfig5 --file kscreenlockerrc --group 'Greeter' --group 'LnF' --group 'General' --key 'showMediaControls' --type 'bool' 'false'
-
-# Start desktop with an empty session
-kwriteconfig5 --file ksmserverrc --group 'General' --key 'loginMode' 'default'
-
-# Open new documents in tabs.
-kwriteconfig5 --file okularpartrc --group 'General' --key 'ShellOpenFileInTabs' --type 'bool' 'true'
-
-# Make yakuake full-width.
-kwriteconfig5 --file yakuakerc --group 'Window' --key 'Width' '100'
-
-# Make yakuake animation instant.
-kwriteconfig5 --file yakuakerc --group 'Animation' --key 'Frames' '0'
-
-# Yakuake keyboard shortcuts.
-# Make it more like tabs in a web browser.
-kwriteconfig5 --file yakuakerc --group 'Shortcuts' --key 'next-session' 'Ctrl+PgDown'
-kwriteconfig5 --file yakuakerc --group 'Shortcuts' --key 'previous-session' 'Ctrl+PgUp'
-kwriteconfig5 --file yakuakerc --group 'Shortcuts' --key 'view-full-screen' 'F11'
-
-# Allow empty clipboard.
-kwriteconfig5 --file klipperrc --group 'General' --key 'PreventEmptyClipboard' --type bool 'false'
-
-# Clear history on exit.
-kwriteconfig5 --file klipperrc --group 'General' --key 'KeepClipboardContents' --type bool 'false'
-
+# Enable pipewire, pipewire-pulse and wireplumber globally
+sudo systemctl --global enable pipewire.service pipewire-pulse.service wireplumber.service
 echo
 echo "Done!"
 echo
